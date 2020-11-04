72ed48efbef884f27ff5ee2e42694fdd237be8dc
Daniel Lima <danieldeveloper1@gmail.com>
2020-11-04 14:15:29 -0300

    ajuste na exibição dos gráficos

--- src/Components/User/UserStatsGraphs.js
+++ src/Components/User/UserStatsGraphs.js
@@ -13,10 +13,14 @@ const UserStatsGraphs = ({ data }) => {
         y: Number(item.acessos),
       };
     });
-
-    setTotal(
-      data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b),
-    );
+    
+    if(graphData.length > 0) {
+      setTotal(
+        data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b),
+      );
+    }
+    
+    
     setGraph(graphData);
   }, [data]);
 
